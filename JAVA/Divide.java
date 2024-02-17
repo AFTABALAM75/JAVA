6d2e4529ccf8bb071a6cd3c8f2f408c5946fe9d4
AFTABALAM75 <145189394+AFTABALAM75@users.noreply.github.com>
2024-02-17 22:58:01 +0530

    Add files via upload

---
+++ JAVA/Divide.java
@@ -0,0 +1,9 @@
+public class Divide {
+    public static void main(String[] args) {
+        int firstNumber = 6;
+        int secondNumber = 3;
+
+        System.out.println(firstNumber + " / " + secondNumber + " = " + (firstNumber / secondNumber));
+
+    }
+}
---
+++ JAVA/Subtract.java
@@ -0,0 +1,14 @@
+public class Subtract {
+
+    public static void main(String[] args) {
+
+        int x = 50;
+        int y = 10;
+
+        int subtract;
+
+        subtract = x - y;
+
+        System.out.println("The substraction of two Numbers: " + subtract);
+    }
+}
