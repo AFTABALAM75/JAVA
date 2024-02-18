01ca1b6892e2b5ec9962d68ecbd09ee372a555d8
AFTABALAM75 <145189394+AFTABALAM75@users.noreply.github.com>
2024-02-18 08:26:15 +0530

    Sum.java

--- JAVA/Sum.java
+++ JAVA/Sum.java
@@ -4,7 +4,7 @@ public class Sum
 
     public static void main(String args[]) {
         int n1 = 10,
-                n2 = 20, sum;
+                n2 = 20, 
         sum = n1 + n2;
         System.out.println("The sum of numbers is: " + sum);
     }
